b3c80dc84cb0fbe503242af42281911be4214777
Rahul Sani <rahulsani@Rahuls-MacBook-Pro.local>
2020-09-11 10:38:58 +0530

    Fixed Scan API

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def allowed_file(filename):
 
 def get_config_data(key):
 	check_deps = configparser.ConfigParser()
-	check_deps.read('adhrit/config')                                         ####
+	check_deps.read('adhrit/config')                                         
 	return check_deps.get('config-data', str(key))
 
 def update_scanid():
@@ -54,12 +54,14 @@ def scan():
 			os.rename(source, dest)
 			
 		  
-			# main()
+			main()
 			thesid = get_config_data('scan_id')
-			rows = getreport(thesid)
-			print(rows)
-			response = rows, {'Access-Control-Allow-Origin': '*'}  
-			# update_scanid()
+			print(thesid)
+			response = getreport(thesid)
+			# print(type(response))
+			# print(response)
+
+			update_scanid()
 			return  response
 	return jsonify(status_msg="apk not sent properly")
 
@@ -74,7 +76,11 @@ def getreport(scan_id):
 		rowarray_list.append(d)
 	# json_data = json.dumps(rowarray_list)
 	# data = json.loads(json_data)
-	data = rowarray_list[0]
+	# data = rowarray_list[0]
+	json_data = rowarray_list
+	data = json_data[0]
+
+	print(data)
 
 	null_key_list = []
 	for key, value in data.items():
@@ -139,91 +145,10 @@ def getreport(scan_id):
 
 @app.route("/testbed")
 def test():
-	sid = 1
-	query = "SELECT * FROM `DataDB` WHERE `ScanId` = '%s'" % sid
-	rows = select_query(query)
-	rowarray_list = []
-	for row in rows:
-		d = dict(zip(row.keys(), row))   
-		rowarray_list.append(d)
-	json_data = rowarray_list
-	data = json_data[0]
-	
-
-	null_key_list = []
-	for key, value in data.items():
-		val_list = eval(value)
-		if not val_list:
-			# print(key)
-			null_key_list.append(key)
-	
-	# print(null_key_list)
-
-	# removing all unused components
-	for key in null_key_list:
-		del data[key]
-		
-	response = {}
-	for key, value in data.items():
-		val_list = eval(value)
-		if key == 'Activity':
-			response.__setitem__(key, val_list)
-			
-		if key == 'ExportedActivity':
-			response.__setitem__(key, val_list)
-		if key == 'BroadcastReceiver':
-			response.__setitem__(key, val_list)
-		if key == 'ExportedReceiver':
-			response.__setitem__(key, val_list)
-		if key == 'Permission':
-			response.__setitem__(key, val_list)
-		if key == 'CriticalPerm':
-			response.__setitem__(key, val_list)
-		if key == 'CustomPerm':
-			response.__setitem__(key, val_list)
-		if key == 'Deeplinks':
-			response.__setitem__(key, val_list)
-		if key == 'Service':
-			response.__setitem__(key, val_list)
-		if key == 'ExportedService':
-			response.__setitem__(key, val_list)
-		if key == 'Taskaffinity':
-			response.__setitem__(key, val_list)
-		if key == 'ImplicitIntent':
-			tmp_imp_intents = []
-			for key in val_list.keys():
-				tmp_imp_intents.extend(val_list[key])
-			key = 'ImplicitIntent'
-			response.__setitem__(key, tmp_imp_intents)
-		if key == 'Provider':
-			tmp_providers = []
-			provider_obj_list = []	
-			for key_parent,value_parent in val_list.items():
-				for key, value in value_parent.items():
-					if key == 'name':
-						temp = key + ' : ' + value
-						provider_obj_list.insert(0, temp)
-					else:
-						temp = key + ' : ' + value
-						provider_obj_list.append(temp)
-				provider_obj_list.append("")
-				tmp_providers.extend(provider_obj_list)
-				provider_obj_list = []
-			key = 'Provider'
-			response.__setitem__(key, tmp_providers)
+	pass
 
 
-
-	# for key, value in data.items():
-	# 	if key == 'Activity':
-	# 		val_list = eval(value)
-	# 		j=[]
-	# 		for i in val_list:
-	# 			j.append(i)
-
-
-
-	return response
+	return 'testBEd'
 
 
 
@@ -240,12 +165,12 @@ def func():
 @app.route("/reset")
 def reset():
 	reset_db()
-	reset_scanid
+	reset_scanid()
 	return jsonify(sts_msg = "Resetted db and Scan Id!")
 
 	
 if __name__ == '__main__':
-	app.run(debug=True, use_reloader=False)
+	app.run(debug=True,)
 
 
 # curl -X POST -F file=@app.apk http://localhost:5000/scan
